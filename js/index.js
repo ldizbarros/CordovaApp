d8b26e1d727c0bf0a57a96040efa39d1e02218bd
ldizbarros <ldizbarros@danielcastelao.org>
2019-03-27 18:18:11 +0000

    Añadir a firebase

---
+++ js/index.js
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+	    this.sacarFoto();
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    },
+
+    sacarFoto: function() {
+    	navigator.camera.getPicture(this.onSuccess, this.onFail, { quality: 25,
+        ºdestinationType: Camera.DestinationType.DATA_URL
+	});
+    },
+    
+    onSuccess: function(imageData){
+    	//var image = document.getElementById('myImage');
+    	//image.src = "data:image/jpeg;base64," + imageData;
+    	enviarFirebase(imageData, "imagen")
+    },
+
+    onFail: function(message) {
+    	alert('Failed because: ' + message);
+    },
+    
+    enviarFirebase: function(file, nombre) {
+        var firebase = require("firebase");
+        var config = {
+            storageBucket: "gs://galeria-aac84.appspot.com/"
+        };
+        firebase.initializeApp(config);
+        var storageRef = firebase.storage().ref();
+        var uploadTask = storageRef.child('images/' + nombre).put(file);
+        uploadTask.on('state_changed', function (snapshot) {
+            console.info(snapshot);
+        }, function (error) {
+            console.error(error);
+        }, function () {
+            var downloadURL = uploadTask.snapshot.downloadURL;
+            console.log(downloadURL);
+    });
+}
+};
+
+app.initialize();
